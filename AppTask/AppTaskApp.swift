10424583c2e6e57d1113886a5e83cd81d6edb5b8
Mohamed Samaha <mohamedsamaha199@gmail.com>
2023-03-07 03:34:22 +0200

    Initial Commit

---
+++ AppTask/AppTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTaskApp.swift
+//  AppTask
+//
+//  Created by Mohamed Samaha on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
